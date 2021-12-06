8b24e1d1b93f1c414ad1d842bdfd2f7f215da297
Jeewon Lee <jeewon.jlee@gmail.com>
2021-12-06 15:05:20 -0500

    Update App component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import "./App.css";
+import PlayerList from "./components/PlayerList";
+import MatchList from "./components/MatchList";
 
 function App() {
-  <div className="App"></div>;
+  return (
+    <div className="App">
+      <h1>
+        Tourney Matches{" "}
+        <span>Where Coding and Tournaments found their Match!</span>
+      </h1>
+      <PlayerList />
+      <MatchList />
+    </div>
+  );
 }
 
 export default App;
